2f9125dc2e558ba56a33e479e4db23ffaa7fe28f
James Moreira de Sousa Junior <jamesmsjr@gmail.com>
2013-09-04 16:32:37 -0300

    Licao 2 adicionada, sobre seletores.

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,26 @@
-/* Certifica-se de rodar seus scripts apenas após o total carregamento do documento HTML */
-jQuery('document').ready( function($){
+jQuery(document).ready(function($) {
+	
+	var p1, p2, p3, btn;
 
-	/* Localiza e guarda referência à tag <body> */
-	var b = $('body');
-	
+	p1 = $('#p1');
+	p2 = $('#p2');
+	p3 = $('#p3');
+	btn = $('button');
 
-	/* Loga no conole os filhos de <body> */
-	// window.console.log(b.children());
+	btn.on('click', function() {
+		modificar_segundo_p();
+	});
 
-	/* Guarda na variável pes (array) todos os <p> cujo ID seja segundo_p */
-	// var pes = b.find("p#segundo_p");
-	
-	/* 
-		Exemplo de seletor complexo. Guarda na variável pes (array) todos os
-		<p> que contenham a classe .par e que em seu conteúdo exista a palavra 
-		"Segundo" 
-	*/
-	// var pes = b.find('p.par:contains("Segundo")');
-	
-	/* Localiza todos os <p> que possuem a classe .par */
-	var pes = b.find('p.par');
-	
-	/* Loga no Firebug a var pes */
-	window.console.log(b);
+	var modificar_segundo_p = function() {
+		p2.text('Texto Modificado!');
 
-	/* Usa o método each() da jQuery para fazer um loop num array javascript
-	e loga no console o conteúdo de cada elemento encontrado no array. Neste caso,
-	tags <p> */
-	b.each( function( index, item ){
-		window.console.log( $(item).text() );
-	} );
-	
-} );
+		p2.html(p2.text() + '<br />Super Teste de HTML!');
+		p2.attr('tittle', 'Simples teste de Título!');
+		p2.css('background-color', 'orange');
+	};
+
+	var ini = function() {
+		alert('jQuery é para poucos!');
+	}();
+
+});
